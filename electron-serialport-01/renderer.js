f4690d3bb211c3c28d81d2bb30337cecc2d233ff
yuzi_01 <40277738@qq.com>
2020-11-03 10:48:50 +0800

    nodejs串口

---
+++ electron-serialport-01/renderer.js
@@ -0,0 +1,24 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const serialport = require('serialport')
+const tableify = require('tableify')
+serialport.list().then(function(ports,err) {
+
+  console.log('ports', ports);
+  if (err) {
+    document.getElementById('error').textContent = err.message
+    return
+  } else {
+    document.getElementById('error').textContent = ''
+  }
+
+  if (ports.length === 0) {
+    document.getElementById('error').textContent = 'No ports discovered'
+  }
+
+  tableHTML = tableify(ports)
+  document.getElementById('ports').innerHTML = tableHTML
+});
+
